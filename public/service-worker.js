4f2acf60a3326c23529707b295c8e34fd8130731
Emin Muhammadi <muemin17631@sabah.edu.az>
2020-04-27 18:55:33 +0400

    prod changed

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,11 +2,13 @@ const CACHE = "emee-ga-v0.0.1";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
 
+
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
 });
+
 
 workbox.routing.registerRoute(
   new RegExp('/*'),

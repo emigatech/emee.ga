c796254430fcf8379d3353e9fd6a142cedc46709
Emin Muhammadi <muemin17631@sabah.edu.az>
2020-04-27 12:53:02 +0400

    Bugs Fixed

--- api/v1/api.php
+++ api/v1/api.php
@@ -47,6 +47,8 @@
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET, POST");
 
+    if(($_DATA === false) || empty($_DATA)) { $_DATA = "";}
+
     print(json_encode([
 
     	'API' => 'eminmuhammadi/HideMyData',
